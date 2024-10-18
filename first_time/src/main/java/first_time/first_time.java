2503dbf6e0cd044690276dbbf7780c88022e5674
Akash Singh <akash.sngg99@gmail.com>
2024-10-18 21:57:26 +0530

    checking

--- first_time/src/main/java/first_time/first_time.java
+++ first_time/src/main/java/first_time/first_time.java
@@ -1,6 +1,9 @@
 package first_time;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class first_time {
@@ -13,15 +16,24 @@ public class first_time {
         EdgeDriver driver = new EdgeDriver();
         
         // Open the URL
-        driver.get("https://github.com/parikshit223933/Coding-Ninjas-Competitive-Programming");
+        //driver.get("https://github.com/parikshit223933/Coding-Ninjas-Competitive-Programming");
         //System.out.println(driver.getTitle());
         
         
-        driver.findElement(By.xpath("//*[@id=\":R55ab:\"]/span/span[2]")).click();
-        for(int i=0;i<5000;i++) {
-        driver.findElement(By.xpath("//*[@id=\":rb:--label\"]")).click();
-        i++;
-        }
+        //driver.findElement(By.xpath("//*[@id=\":R55ab:\"]/span/span[2]")).click();
+        //for(int i=0;i<5000;i++) {
+        //driver.findElement(By.xpath("//*[@id=\":rb:--label\"]")).click();
+        //i++;
+        //}
+        driver.get("https://cosmocode.io/automation-practice-webtable/");
+		
+		List<WebElement> val = (List<WebElement>) driver.findElements(By.xpath("//table[@id='countries']/tbody/tr/td[2]"));
+		//List<WebElement> vald=driver.findElements(By.xpath("//table[@id='countries']/tbody/tr/td[2]"));
+        
+		for(WebElement vals : val) {
+			System.out.println(vals.getText());
+		}
+		driver.close();
     }
 
 }
